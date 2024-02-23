6b93cc5f47d95f09a573f847425f2bb03277d456
richardcudd <root@ip-172-31-30-30.eu-west-1.compute.internal>
2024-02-23 13:59:27 +0000

    add replicator config

---
+++ configure-replicator.sh
@@ -0,0 +1,50 @@
+# Create the test-topic
+docker-compose exec srcKafka kafka-topics --bootstrap-server srcKafka:19092 --topic topic-test --create --partitions 1 --replication-factor 1
+
+
+HEADER="Content-Type: application/json"
+DATA=$( cat << EOF
+{
+  "name": "testReplicator",
+  "config": {
+    "name": "testReplicator",
+    "connector.class": "io.confluent.connect.replicator.ReplicatorSourceConnector",
+    "tasks.max": "1",
+    "confluent.topic.replication.factor": "1",
+    "producer.override.enable.idempotence": "true",
+    "producer.override.acks": "all",
+    "producer.override.max.in.flight.requests.per.connection": "1",
+
+    "topic.whitelist": "topic-test",
+
+    "src.kafka.bootstrap.servers": "srcKafka:19092",
+    "src.key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "src.key.converter.schemas.enable": "false",
+    "src.value.converter": "io.confluent.connect.avro.AvroConverter",
+    "src.value.converter.schema.registry.url": "http://srcSchemaregistry:8085",
+
+    "dest.kafka.bootstrap.servers": "destKafka:29092",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "key.converter.schemas.enable": "false",
+    "value.converter": "io.confluent.connect.avro.AvroConverter",
+    "value.converter.schema.registry.url": "http://destSchemaregistry:8086"
+  }
+}
+EOF
+)
+
+
+RETCODE=1
+while [ $RETCODE -ne 0 ]
+do
+  curl -f -X POST -H "${HEADER}" --data "${DATA}" http://localhost:8083/connectors
+  RETCODE=$?
+  if [ $RETCODE -ne 0 ]
+  then
+    echo "Failed to submit replicator to Connect. This could be because the Connect worker is not yet started. Will retry in 10 seconds"
+  fi
+  #backoff
+  sleep 10
+done
+echo "replicator configured"
+
